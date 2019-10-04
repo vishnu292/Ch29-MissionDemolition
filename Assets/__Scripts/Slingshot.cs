11523bae23ffa79b95c256ce68939bb5769e3347
vishnu292 <47337198+vishnu292@users.noreply.github.com>
2019-10-04 16:53:04 -0400

    Programming SlingShot
    no observable changes?

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 // YOU must implement the Slingshot
 
 public class Slingshot : MonoBehaviour {
-  
 
-// Place class variables here
 
+    // Place class variables here
+    GameObject launchPoint;
 
 
     private void Awake()
     {
-
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter() {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
+    void OnMouseExit() {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
 
     private void Update()
     {
